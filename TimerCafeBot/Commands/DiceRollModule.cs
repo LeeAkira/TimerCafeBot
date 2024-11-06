edb108fbec84ad9ebb0b9b3ec7eaf41f5a0b373e
Leandro <leandro.a.shimokawa@gmail.com>
2024-11-05 21:14:17 -0300

    Dice Roll XdY format

--- TimerCafeBot/Commands/DiceRollModule.cs
+++ TimerCafeBot/Commands/DiceRollModule.cs
@@ -5,10 +5,28 @@ namespace TimerCafeBot.Commands;
 public class DiceRollModule : ModuleBase<SocketCommandContext>
 {
     [Command("roll")]
-    public async Task DiceRoll()
+    [Summary("Rolls dices")]
+    public async Task DiceRoll([Summary("Type 'roll XdY' where X is the number of dices and Y is the amount of faces")] string dices)
     {
-        var random = new Random();
-        var result = random.Next(6)+1;
-        await ReplyAsync(result.ToString());
+        try
+        {
+            var sdices = dices.Split('d');
+            int amount = int.Parse(sdices[0]);
+            int faces = int.Parse(sdices[1]);
+            var random = new Random();
+            string result = "";
+
+            for (int i = 0; i < amount; i += 1) {
+                if (i > 0)
+                    result += ", ";
+                result += random.Next(faces) + 1;
+            }
+
+            await ReplyAsync(result.ToString());
+        }
+        catch(FormatException)
+        {
+            await ReplyAsync("Incorrect Format");
+        }
     }
 }
